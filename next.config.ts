ee6b84d383dfe92b146dea1ead3b092a123e73da
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-08-24 23:00:52 -0300

    chore(config): add external images domain and dynamic dev origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: process.env.DEV_ORIGIN ? [process.env.DEV_ORIGIN] : [],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
