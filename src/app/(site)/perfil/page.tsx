1e636ffd6bd7fdbc2b12e85cdea750860e434343
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-08-29 15:02:01 -0300

    feat(auth): protect profile and orders routes with proxy

---
+++ src/app/(site)/perfil/page.tsx
@@ -0,0 +1,5 @@
+export default async function ProfilePage() {
+  return (
+    <div>....</div>
+  )
+}
---
+++ src/proxy.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+
+  const token = request.cookies.get('auth_token')?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/entrar', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/pedidos/:path*', '/perfil/:path*'],
+}
